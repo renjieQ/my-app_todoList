2910f2b65e9e8fe4441289a7a1b90d2bd4f6d6a1
renjieQ <qurenjie@qq.com>
2021-08-20 16:43:54 +0800

    3

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -9,15 +9,12 @@ function TodoList() {
     if (!todo.text || /^\s*$/.test(todo.test)) {
       return;
     }
-
     const newTodos = [todo, ...todos];
-
     setTodos(newTodos);
   };
 
   const removeTodo = id => {
     const removeArr = [...todos].filter(todo => todo.id !== id);
-
     setTodos(removeArr);
   };
 
